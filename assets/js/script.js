639400a2709fa62de824377ce42533d9b8215f90
cemilhancakar <cemilhancakarcmtal@gmail.com>
2023-01-06 20:17:38 +0300

    hata giderildi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,7 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".uil-times");
-const menuIcon = document.querySelector(".uil-bars");
-closeIcon.style.display = "none";
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
+function openNav() {
+  document.getElementById("YanMenu").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("YanMenu").style.width = "0";
 }
